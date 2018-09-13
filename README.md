2587624096809dbe29b1b4af985cabacbaf26219
URenko <18209292+URenko@users.noreply.github.com>
2018-09-13 16:13:27 +0800

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Access-zhwiki_demo
+A demo for people under GFW to access zh.wikipedia.org  
+Test only in python3.7
+## Idea
+HTTP(zh.wikipedia.org) over TLS(www.wikipedia.org)
+## LICENSE
+MIT
---
+++ main.py
@@ -0,0 +1,31 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from http.server import *
+import http.client
+
+class ZhwikiHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.log_request()
+		del self.headers['Host']
+		self.headers['Host'] = 'zh.wikipedia.org'
+		remote = http.client.HTTPSConnection("www.wikipedia.org")
+		remote.request(self.command, self.path, headers=self.headers)
+		res = remote.getresponse()
+		print(res.status, res.reason)
+		self.send_response(res.status, res.reason)
+		for header, value in res.getheaders():
+			value = value.replace('https://zh.wikipedia.org','http://127.0.0.1')
+			self.send_header(header, value)
+		self.end_headers()
+		self.wfile.write(res.read().replace(b'https://zh.wikipedia.org',b'http://127.0.0.1').replace(b'zh.wikipedia.org',b'127.0.0.1'))
+		remote.close()
+
+server_address = ('127.0.0.1', 80)
+httpd = ThreadingHTTPServer(server_address, ZhwikiHandler)
+httpd.serve_forever()
